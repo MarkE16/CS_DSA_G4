3a83d95f2d5e2cdaa165528162040f0f475410a0
PaceDeLeon <hvd16@txstate.edu>
2024-12-03 21:25:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-
+//for main we can just do .insert instead of reading in user input (if we are running low on time)
+//since that still follows the skeleton code for assignment. If you have time to impliment user input
+//that works too
 int main() {
   // Code here...
   return 0;
